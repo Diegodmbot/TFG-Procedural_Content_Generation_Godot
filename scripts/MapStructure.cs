8d95022be75a218254bdd543e7acd4bd9e9bdad5
Diego Díaz Morón <alu0101337975@ull.edu.es>
2024-04-15 21:06:19 +0100

    Estructura de datos mejorada

--- scripts/MapStructure.cs
+++ scripts/MapStructure.cs
@@ -31,7 +31,7 @@ public partial class MapStructure : Node2D
 	TileMap DungeonTileMap;
 	// map size
 	private System.Numerics.Vector2 _borders;
-	public List<byte[,]> Structure = [];
+	public byte[,,] Structure;
 	// Si el número en la posición [i,j] es diferente a 0 significa que las habitaciones i y j están conectadas
 	byte[,] Neighborhood;
 	// Guarda la posición de las puertas de cada habitación
@@ -47,11 +47,7 @@ public partial class MapStructure : Node2D
 		DungeonTileMap.Clear();
 		int pointsCount = VoronoiDiagram.PointsLimit + 1;
 		_borders = new(ExportedBorders.X, ExportedBorders.Y);
-		// Lo sé... si lo cambio la escena no se carga. Simplemente ignoralo, NO LO MIRES!!!
-		Structure.Add(new byte[(int)_borders.X, (int)_borders.Y]);
-		Structure.Add(new byte[(int)_borders.X, (int)_borders.Y]);
-		Structure.Add(new byte[(int)_borders.X, (int)_borders.Y]);
-		Structure.Add(new byte[(int)_borders.X, (int)_borders.Y]);
+		Structure = new byte[(int)_borders.X, (int)_borders.Y, 4];
 		Neighborhood = new byte[pointsCount, pointsCount];
 		DoorsPositions = Enumerable.Range(0, pointsCount)
 														.Select(_ => new List<System.Numerics.Vector2>())
@@ -77,7 +73,7 @@ public partial class MapStructure : Node2D
 		{
 			for (int j = 0; j < _borders.Y; j++)
 			{
-				if (Structure[(int)MapType.GROUND][i, j] == roomId)
+				if (Structure[i, j, (int)MapType.GROUND] == roomId)
 				{
 					structure.Add(new Vector2(i, j));
 				}
@@ -96,7 +92,7 @@ public partial class MapStructure : Node2D
 			layer[i].Resize((int)_borders.Y);
 			for (int j = 0; j < layer[i].Count; j++)
 			{
-				layer[i][j] = Structure[layerId][i, j];
+				layer[i][j] = Structure[i, j, layerId];
 			}
 		}
 		return layer;
@@ -110,7 +106,7 @@ public partial class MapStructure : Node2D
 		{
 			for (int j = 0; j < DoorsPositions[i].Count; j++)
 			{
-				int doorId = Structure[(int)MapType.DOORS][(int)DoorsPositions[i][j].X, (int)DoorsPositions[i][j].Y];
+				int doorId = Structure[(int)DoorsPositions[i][j].X, (int)DoorsPositions[i][j].Y, (int)MapType.DOORS];
 				Vector2 doorPosition = new((int)DoorsPositions[i][j].X, (int)DoorsPositions[i][j].Y);
 				doors[doorId] = doorPosition;
 			}
@@ -126,7 +122,7 @@ public partial class MapStructure : Node2D
 		{
 			for (int j = 0; j < SpawnPositions[i].Count; j++)
 			{
-				int id = Structure[(int)MapType.DOORS][(int)SpawnPositions[i][j].X, (int)SpawnPositions[i][j].Y];
+				int id = Structure[(int)SpawnPositions[i][j].X, (int)SpawnPositions[i][j].Y, (int)MapType.DOORS];
 				Vector2 position = new((int)SpawnPositions[i][j].X, (int)SpawnPositions[i][j].Y);
 				spawns[id] = position;
 			}
@@ -136,14 +132,20 @@ public partial class MapStructure : Node2D
 
 	private int GetRoomByPosition(int coord_x, int coord_y)
 	{
-		return Structure[(int)MapType.AREA][coord_x, coord_y];
+		return Structure[coord_x, coord_y, (int)MapType.AREA];
 	}
 
 
 	private void CreateRooms()
 	{
 		var map = VoronoiDiagram.BuildVoronoiDiagram(_borders);
-		Structure[(int)MapType.AREA] = map;
+		for (int i = 0; i < _borders.X; i++)
+		{
+			for (int j = 0; j < _borders.Y; j++)
+			{
+				Structure[i, j, (int)MapType.AREA] = map[i, j];
+			}
+		}
 	}
 
 	private void GenerateBorders()
@@ -154,24 +156,24 @@ public partial class MapStructure : Node2D
 			{
 				if (i == 0 || i == _borders.X - 1 || j == 0 || j == _borders.Y - 1)
 				{
-					Structure[(int)MapType.WALLS][i, j] = 1;
+					Structure[i, j, (int)MapType.WALLS] = 1;
 				}
 				else
 				{
 					foreach (var direction in Directions)
 					{
-						byte neighbor = Structure[(int)MapType.AREA][i + (int)direction.X, j + (int)direction.Y];
-						if (neighbor != Structure[(int)MapType.AREA][i, j] && neighbor != 0)
+						byte neighbor = Structure[i + (int)direction.X, j + (int)direction.Y, (int)MapType.AREA];
+						if (neighbor != Structure[i, j, (int)MapType.AREA] && neighbor != 0)
 						{
 							// El muro guarda el id de la habitación adyacente
-							Structure[(int)MapType.WALLS][i, j] = neighbor;
-							Neighborhood[Structure[(int)MapType.AREA][i, j], neighbor] = (byte)NeighborType.NEIGHBOORS;
+							Structure[i, j, (int)MapType.WALLS] = neighbor;
+							Neighborhood[Structure[i, j, (int)MapType.AREA], neighbor] = (byte)NeighborType.NEIGHBOORS;
 							break;
 						}
 					}
-					if (Structure[(int)MapType.WALLS][i, j] == 0)
+					if (Structure[i, j, (int)MapType.WALLS] == 0)
 					{
-						RoomsSurface[Structure[(int)MapType.AREA][i, j]].area += 1;
+						RoomsSurface[Structure[i, j, (int)MapType.AREA]].area += 1;
 					}
 				}
 			}
@@ -202,34 +204,34 @@ public partial class MapStructure : Node2D
 			int doorX = random.Next(1, (int)_borders.X - 1);
 			int doorY = random.Next(1, (int)_borders.Y - 1);
 			// La casilla tiener que ser un muro
-			if (Structure[(int)MapType.WALLS][doorX, doorY] != 0)
+			if (Structure[doorX, doorY, (int)MapType.WALLS] != 0)
 			{
 				foreach (var direction in Directions)
 				{
 					int adjacentRoomX = doorX + (int)direction.X;
 					int adjacentRoomY = doorY + (int)direction.Y;
-					if (Structure[(int)MapType.WALLS][adjacentRoomX, adjacentRoomY] == 0)
+					if (Structure[adjacentRoomX, adjacentRoomY, (int)MapType.WALLS] == 0)
 					{
 						int oppositeRoomX = doorX - (int)direction.X * 2;
 						int oppositeRoomY = doorY - (int)direction.Y * 2;
-						if (oppositeRoomX > 0 && oppositeRoomX < _borders.X && oppositeRoomY > 0 && oppositeRoomY < _borders.Y && Structure[(int)MapType.WALLS][oppositeRoomX, oppositeRoomY] == 0)
+						if (oppositeRoomX > 0 && oppositeRoomX < _borders.X && oppositeRoomY > 0 && oppositeRoomY < _borders.Y && Structure[oppositeRoomX, oppositeRoomY, (int)MapType.WALLS] == 0)
 						{
-							if (Neighborhood[Structure[(int)MapType.AREA][adjacentRoomX, adjacentRoomY], Structure[(int)MapType.AREA][oppositeRoomX, oppositeRoomY]] == 2)
+							if (Neighborhood[Structure[adjacentRoomX, adjacentRoomY, (int)MapType.AREA], Structure[oppositeRoomX, oppositeRoomY, (int)MapType.AREA]] == 2)
 							{
 								// Las puertas tienen un número par y los suelos de las habitaciones opuetas son impares
-								Structure[(int)MapType.DOORS][doorX, doorY] = doorId;
-								Structure[(int)MapType.DOORS][adjacentRoomX, adjacentRoomY] = doorId++;
-								Structure[(int)MapType.DOORS][doorX - (int)direction.X, doorY - (int)direction.Y] = doorId;
-								Structure[(int)MapType.DOORS][oppositeRoomX, oppositeRoomY] = doorId++;
+								Structure[doorX, doorY, (int)MapType.DOORS] = doorId;
+								Structure[adjacentRoomX, adjacentRoomY, (int)MapType.DOORS] = doorId++;
+								Structure[doorX - (int)direction.X, doorY - (int)direction.Y, (int)MapType.DOORS] = doorId;
+								Structure[oppositeRoomX, oppositeRoomY, (int)MapType.DOORS] = doorId++;
 								// Guardar la vencidad
-								Neighborhood[Structure[(int)MapType.AREA][adjacentRoomX, adjacentRoomY], Structure[(int)MapType.AREA][oppositeRoomX, oppositeRoomY]] = (byte)NeighborType.DOORS;
-								Neighborhood[Structure[(int)MapType.AREA][oppositeRoomX, oppositeRoomY], Structure[(int)MapType.AREA][adjacentRoomX, adjacentRoomY]] = (byte)NeighborType.DOORS;
+								Neighborhood[Structure[adjacentRoomX, adjacentRoomY, (int)MapType.AREA], Structure[oppositeRoomX, oppositeRoomY, (int)MapType.AREA]] = (byte)NeighborType.DOORS;
+								Neighborhood[Structure[oppositeRoomX, oppositeRoomY, (int)MapType.AREA], Structure[adjacentRoomX, adjacentRoomY, (int)MapType.AREA]] = (byte)NeighborType.DOORS;
 								// Guardar la posicion de las puertas
-								DoorsPositions[Structure[(int)MapType.AREA][adjacentRoomX, adjacentRoomY]].Add(new System.Numerics.Vector2(doorX, doorY));
-								DoorsPositions[Structure[(int)MapType.AREA][oppositeRoomX, oppositeRoomY]].Add(new System.Numerics.Vector2(doorX - (int)direction.X, doorY - (int)direction.Y));
+								DoorsPositions[Structure[adjacentRoomX, adjacentRoomY, (int)MapType.AREA]].Add(new System.Numerics.Vector2(doorX, doorY));
+								DoorsPositions[Structure[oppositeRoomX, oppositeRoomY, (int)MapType.AREA]].Add(new System.Numerics.Vector2(doorX - (int)direction.X, doorY - (int)direction.Y));
 								// Guardar la posición del spawn
-								SpawnPositions[Structure[(int)MapType.AREA][adjacentRoomX, adjacentRoomY]].Add(new System.Numerics.Vector2(adjacentRoomX, adjacentRoomY));
-								SpawnPositions[Structure[(int)MapType.AREA][oppositeRoomX, oppositeRoomY]].Add(new System.Numerics.Vector2(oppositeRoomX, oppositeRoomY));
+								SpawnPositions[Structure[adjacentRoomX, adjacentRoomY, (int)MapType.AREA]].Add(new System.Numerics.Vector2(adjacentRoomX, adjacentRoomY));
+								SpawnPositions[Structure[oppositeRoomX, oppositeRoomY, (int)MapType.AREA]].Add(new System.Numerics.Vector2(oppositeRoomX, oppositeRoomY));
 								break;
 							}
 						}
@@ -264,11 +266,11 @@ public partial class MapStructure : Node2D
 			roomCreated = (double)RoomsSurface[roomId].ground / RoomsSurface[roomId].area > MinimumGroundPerRoom && PathConnected(automatas[0], roomId);
 			for (int j = 0; j < automatas.Count; j++)
 			{
-				if (Structure[(int)MapType.GROUND][(int)automatas[j].X, (int)automatas[j].Y] == 0)
+				if (Structure[(int)automatas[j].X, (int)automatas[j].Y, (int)MapType.GROUND] == 0)
 				{
 					RoomsSurface[roomId].ground += 1;
 				}
-				Structure[(int)MapType.GROUND][(int)automatas[j].X, (int)automatas[j].Y] = (byte)roomId;
+				Structure[(int)automatas[j].X, (int)automatas[j].Y, (int)MapType.GROUND] = (byte)roomId;
 				automatas[j] = MoveAutomata(automatas[j]);
 			}
 		}
@@ -279,7 +281,7 @@ public partial class MapStructure : Node2D
 		Random random = new();
 		int randomNumber = random.Next(0, 4);
 		System.Numerics.Vector2 targetPosition = automata + Directions[randomNumber];
-		while (Structure[(int)MapType.WALLS][(int)targetPosition.X, (int)targetPosition.Y] != 0)
+		while (Structure[(int)targetPosition.X, (int)targetPosition.Y, (int)MapType.WALLS] != 0)
 		{
 			randomNumber = random.Next(0, 4);
 			targetPosition = automata + Directions[randomNumber];
@@ -302,7 +304,7 @@ public partial class MapStructure : Node2D
 				System.Numerics.Vector2 neighbor = current + direction;
 				if (neighbor.X >= 0 && neighbor.X < _borders.X && neighbor.Y >= 0 && neighbor.Y < _borders.Y)
 				{
-					if (Structure[(int)MapType.GROUND][(int)neighbor.X, (int)neighbor.Y] != 0 && !visited[(int)neighbor.X, (int)neighbor.Y])
+					if (Structure[(int)neighbor.X, (int)neighbor.Y, (int)MapType.GROUND] != 0 && !visited[(int)neighbor.X, (int)neighbor.Y])
 					{
 						queue.Enqueue(neighbor);
 						visited[(int)neighbor.X, (int)neighbor.Y] = true;
@@ -324,10 +326,10 @@ public partial class MapStructure : Node2D
 		{
 			for (int j = 0; j < _borders.Y; j++)
 			{
-				if (Structure[(int)MapType.AREA][i, j] == roomId)
+				if (Structure[i, j, (int)MapType.AREA] == roomId)
 				{
 
-					if (Structure[(int)MapType.GROUND][i, j] == 0)
+					if (Structure[i, j, (int)MapType.GROUND] == 0)
 					{
 						DungeonTileMap.SetCell(0, new Vector2I(i, j), 2, Vector2I.Zero);
 					}
@@ -363,7 +365,7 @@ public partial class MapStructure : Node2D
 		{
 			for (int j = 0; j < _borders.Y; j++)
 			{
-				if (Structure[(int)MapType.AREA][i, j] == roomId)
+				if (Structure[i, j, (int)MapType.AREA] == roomId)
 				{
 					DungeonTileMap.SetCell(1, new Vector2I(i, j));
 				}
